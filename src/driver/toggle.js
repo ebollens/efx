42b6c91a54698f74d16f65dae2883bc7fd9c62af
Eric Bollens <ebollens@ucla.edu>
2012-11-17 15:43:33 -0800

    Add support for slide-type toggle

--- src/driver/toggle.js
+++ src/driver/toggle.js
@@ -5,9 +5,21 @@
         var container = data.container,
             target    = data.target,
             trigger   = data.trigger;
-            
+    
+        var hasModifier = false;
+        $.each(['slide'], function(){
+            if(_.inState(container, this+''))
+                hasModifier = true;
+        })
+        
+        if(!hasModifier)
+            _.addState(container, 'basic')
+        
         if(!_.inState(target, 'show'))
             _.addState(target, 'hide')
+        
+        if(_.inState(container, 'slide') && _.inState(target, 'hide'))
+            target.hide();
 
     });
 
@@ -19,11 +31,21 @@
             
             
         if(_.inState(target, 'show')){
+            
             _.removeState(target, 'show')
             _.addState(target, 'hide')
+            
+            if(_.inState(container, 'slide'))
+                target.slideUp();
+            
         }else{
+            
             _.removeState(target, 'hide')
             _.addState(target, 'show')
+            
+            if(_.inState(container, 'slide'))
+                target.slideDown();
+            
         }
 
     });
